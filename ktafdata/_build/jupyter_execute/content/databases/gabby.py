5c710e16a421e5fddf85c7bb6f9d4be6b9049a05
cristinabaldor <ccbaldor@gmail.com>
2021-10-22 18:15:42 -0400

    with dataframe

--- ktafdata/_build/jupyter_execute/content/databases/gabby.py
+++ ktafdata/_build/jupyter_execute/content/databases/gabby.py
@@ -7,3 +7,40 @@
 # 
 
 # 
+
+# In[1]:
+
+
+#Sortable dataframe tools
+import pandas as pd
+import qgrid
+import pivottablejs
+
+
+# In[2]:
+
+
+file = 'all_columns.csv'
+columns_df=pd.read_csv(file)
+
+
+# In[3]:
+
+
+#Regular pandas view of all the data
+columns_df
+
+
+# In[4]:
+
+
+#Sortable/searchable list with qgrid
+qgrid.QgridWidget(df=columns_df)
+qgrid
+
+
+# In[ ]:
+
+
+
+
